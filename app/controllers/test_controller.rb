5a68fd128b193c8421ac093651d94251a078815f
Rodrigo Sampaio Vaz <rodrigo.vaz@gmail.com>
2014-11-19 18:03:40 -0200

    no database

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -6,14 +6,15 @@ class TestController < ApplicationController
   def start
     mem_start = `ps -o rss= -p #{Process.pid}`.to_i
     a = []
-    1000000.times do
+    100000.times do
       a << Object.new
     end
     mem_alloc = `ps -o rss= -p #{Process.pid}`.to_i
     a = nil
     GC.start
     mem_after_gc = `ps -o rss= -p #{Process.pid}`.to_i
-    render json: {mem_start: mem_start,
+    render json: {timestamp: Time.now,
+                  mem_start: mem_start,
                   mem_allocated: mem_alloc,
                   mem_after_gc: mem_after_gc}
   end
